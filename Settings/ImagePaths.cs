b6bc59b6d2b97ae58ed0beef43aa394d651ec303
Luis J. Montes <luisjavm3@gmail.com>
2022-07-08 11:39:52 -0500

    Add ImagePath to application setting

---
+++ Settings/ImagePaths.cs
@@ -0,0 +1,9 @@
+namespace Disney.Settings
+{
+    public class ImagePaths
+    {
+        public string Characters { get; set; }
+        public string Genres { get; set; }
+        public string MovieSeries { get; set; }
+    }
+}
