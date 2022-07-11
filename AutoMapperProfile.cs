ac0d6dd013b9cf4e3f65cc7233b025e78eef3631
Luis J. Montes <luisjavm3@gmail.com>
2022-07-11 15:28:27 -0500

    Create new map

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -11,6 +11,16 @@ namespace Disney
             CreateMap<Character, CharacterResponseDto>();
             CreateMap<CharacterCreateDto, Character>();
             CreateMap<Character, CharacterListItemDto>();
+            CreateMap<Character, CharacterUpdateDto>();
+
+            CreateMap<Character, CharacterUpdateResponseDto>()
+                .ForMember(x => x.Image, opt => opt.MapFrom(src => GetImage(src.ImagePath)));
+        }
+
+        private string GetImage(string path)
+        {
+            var stream = System.IO.File.ReadAllBytes(path);
+            return Convert.ToBase64String(stream);
         }
     }
 }
