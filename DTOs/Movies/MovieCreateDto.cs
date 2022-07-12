d7146939fdb278d27b4e78197fd5e039b4a35629
Luis J. Montes <luisjavm3@gmail.com>
2022-07-12 15:53:31 -0500

    Implement get and set of Released property

--- DTOs/Movies/MovieCreateDto.cs
+++ DTOs/Movies/MovieCreateDto.cs
@@ -1,17 +1,36 @@
 using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 
 namespace Disney.DTOs.Movies
 {
     public class MovieCreateDto
     {
+        private static int CurrentYear = DateTime.UtcNow.Year;
+        private int _released;
+
         [Required]
         public IFormFile Image { get; set; }
 
         [Required]
         public string Title { get; set; }
 
+        // In 1888 the first movie was released.
         [Required]
-        public int Released { get; set; }
+        [Range(1888, 3000)]
+        public int Released
+        {
+            get
+            {
+                if (_released > DateTime.UtcNow.Year)
+                    return DateTime.UtcNow.Year;
+
+                return _released;
+            }
+            set
+            {
+                _released = value;
+            }
+        }
 
         [Required]
         public int Rate { get; set; }
