25298061240103dced3b58a547db3e6d0e878acc
Luis J. Montes <luisjavm3@gmail.com>
2022-07-13 11:08:07 -0500

    Add ImagePath property to Genre entity and update database scheme

--- Entities/Genre.cs
+++ Entities/Genre.cs
@@ -4,6 +4,9 @@ namespace Disney.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ImagePath { get; set; }
+
+
         public IList<MovieSerie> MovieSeries { get; set; }
     }
 }
---
+++ Migrations/20220713160659_second.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Disney.Migrations
+{
+    public partial class second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Genres",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Genres");
+        }
+    }
+}
