ace9454d8b82b11ffddd8a51b220a9bbb84c98e3
Luis J. Montes <luisjavm3@gmail.com>
2022-07-14 16:12:57 -0500

    Implement SendGrid sending welcome email

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,6 +2,8 @@ using Disney.DTOs.User;
 using Disney.Services;
 using Disney.Settings;
 using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Disney.Controllers
 {
@@ -12,18 +14,21 @@ namespace Disney.Controllers
         private readonly IAuthService _authService;
         private readonly IConfiguration _configuration;
         private readonly JwtSettings _jwtSettings;
+        private readonly SendGridSettings _sendGridSettings;
 
         public AuthController(IAuthService authService, IConfiguration configuration)
         {
             _authService = authService;
             _configuration = configuration;
             _jwtSettings = _configuration.GetSection(nameof(JwtSettings)).Get<JwtSettings>();
+            _sendGridSettings = _configuration.GetSection(nameof(SendGridSettings)).Get<SendGridSettings>();
         }
 
         [HttpPost("Register")]
         public async Task<ActionResult> Register(UserAuthDto authDto)
         {
             await _authService.Register(authDto);
+            await SendWelcomeEmail(authDto.Email);
             return Ok();
         }
 
@@ -44,6 +49,19 @@ namespace Disney.Controllers
             };
 
             Response.Cookies.Append("jwt", jwt, cookieOptions);
+        }
+
+        private async Task SendWelcomeEmail(string email)
+        {
+            var apiKey = _sendGridSettings.NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY;
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(_sendGridSettings.From, "Luis J. Montes");
+            var subject = $"Welcome {email}, now you are registed in Disney Movies App.";
+            var to = new EmailAddress(email, email);
+            var plainTextContent = "Enjoy...";
+            var htmlContent = "<strong>Enjoy...</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
---
+++ Settings/SendGridSettings.cs
@@ -0,0 +1,8 @@
+namespace Disney.Settings
+{
+    public class SendGridSettings
+    {
+        public string NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY { get; set; }
+        public string From { get; set; }
+    }
+}
